0e7eeb4bce08bcb097817d33ead2461b3356636e
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2016-02-14 11:52:39 -0800

    Create init.sh

---
+++ sh/init.sh
@@ -0,0 +1,4 @@
+dnf install zsh vim -y
+git clone https://github.com/xuebaofeng/os-script.git
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
