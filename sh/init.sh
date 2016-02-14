45c58ee15bb78c60fc7dc961f79a688d37971067
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2016-02-14 12:12:14 -0800

    Update init.sh

--- sh/init.sh
+++ sh/init.sh
@@ -2,3 +2,6 @@ dnf install zsh vim -y
 git clone https://github.com/xuebaofeng/os-script.git
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh
+git clone https://github.com/clvv/fasd
+cd fasd 
+sudo make install
