a954bebb37e040a407c572be05114baa4f1acf1c
Baofeng Xue <xbaofeng@gmail.com>
2016-02-26 16:22:55 -0800

    backup

--- sh/backup.sh
+++ sh/backup.sh
@@ -2,13 +2,14 @@
 
 git pull
 
+cd ..
+
 cp ~/.zshrc ../dot/
 cp ~/.gitconfig ../dot/
 cp ~/.gitignore_global ../dot/
 cp ~/.vimrc ../dot/
 
 crontab -l > ../dot/.crontab
-#crontab ../dot/.crontab
 
 git add -A
 git commit -m 'backup'
