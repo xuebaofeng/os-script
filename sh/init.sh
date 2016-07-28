e8f3067a6f5294e81f4b537634c18301d526c090
Baofeng Xue <xbaofeng@gmail.com>
2016-07-28 15:59:09 -0700

    r

--- sh/init.sh
+++ sh/init.sh
@@ -1,11 +1,24 @@
-dnf install zsh vim -y
-git clone https://github.com/xuebaofeng/os-script.git
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#install zsh, git
+
 chsh -s /bin/zsh
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+#install fasd
 git clone https://github.com/clvv/fasd
 cd fasd 
 sudo make install
+
+
+#install classifier
 pip install classifier
 
 #install sdkman
 curl -s "https://get.sdkman.io" | bash
+
+
+#copy config
+git clone https://github.com/xuebaofeng/os-script.git
+cp ~/github/os-script/dot/* .
