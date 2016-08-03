3d926096e08cf0084c36490cfae55c9408546f8d
Baofeng Xue <xbaofeng@gmail.com>
2016-08-03 14:02:43 -0700

    'backup'

--- bat/backup-vm.bat
+++ bat/backup-vm.bat
@@ -1,4 +1,6 @@
 rem https://my.vmware.com/web/vmware/free#desktop_end_user_computing/vmware_workstation_player/12_0|PLAYER-1200|drivers_tools
-vmrun suspend c:\Users\I854966\Downloads\Bizx2.0.1-b1602a\Bizx2.0.vmx
-robocopy C:\Users\I854966\Downloads\Bizx2.0.1-b1602a C:\Users\I854966\Downloads\vm-backup
+
+cd %HOMEPATH%
+vmrun suspend Downloads\Bizx2.0.1-b1602a\Bizx2.0.vmx
+robocopy Downloads\Bizx2.0.1-b1602a Downloads\vm-backup
 pause
--- bat/start-vm.bat
+++ bat/start-vm.bat
@@ -1,3 +1,3 @@
-vmrun start c:\Users\I854966\Downloads\Bizx2.0.1-b1602a\Bizx2.0.vmx
+vmrun start %HOMEPATH%\Downloads\Bizx2.0.1-b1602a\Bizx2.0.vmx
 net start "VMware NAT Service"
 pause
