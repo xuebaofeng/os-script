41129025719e7385fc3f265f91dce0088de9b1f2
Baofeng Xue <xbaofeng@gmail.com>
2016-08-03 13:56:22 -0700

    'backup'

--- bat/schtask.bat
+++ bat/schtask.bat
@@ -2,4 +2,4 @@ schtasks /Delete /TN "backup vm" /F
 schtasks /create /sc WEEKLY /tn "backup vm" /tr "c:\green\bat\backup-vm.bat" /d wed /st 12:00
 
 schtasks /Delete /TN "backup os" /F
-schtasks /create /sc WEEKLY /tn "backup os" /tr "c:\green\bat\backup-os.bat" /d wed /st 12:00
+schtasks /create /sc daily /tn "backup os" /tr "c:\green\bat\backup-os.bat" /st 12:00
