e3c24812fde661133160c6924babad7f4d8307b1
Baofeng Xue <xbaofeng@gmail.com>
2016-08-08 16:00:08 -0700

    backup

--- sh/au.sh
+++ sh/au.sh
@@ -3,5 +3,5 @@ cd /data/sfsf/workspace/trunk/build-system
 svn update
 cd /data/sfsf/workspace/trunk/au-V4
 svn update
-ant deploy
-start_jboss_jrebel_pub_ram.sh
+ant clean-deploy
+start_jboss_pub_ram.sh
