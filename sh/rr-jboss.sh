71f19f9f85ebd55d2658a40bfb7b61ce1553dd26
Baofeng Xue <xbaofeng@gmail.com>
2016-10-30 17:56:37 -0700

    r

--- sh/rr-jboss.sh
+++ sh/rr-jboss.sh
@@ -5,4 +5,4 @@ gradle deploy --stacktrace
 #rm /data/sfsf/workspace/trunk/tomcat-sfs/sf_lib/idl-schedulerservice-service-1.2.302.jar
 #cp /data/sfsf/workspace/ant/schedulerservice/idl-schedulerservice-service/target/schedulerservice/package/idl-schedulerservice-service.jar /data/sfsf/workspace/trunk/tomcat-sfs/sf_lib/
 
-~/restart-jboss.sh
+restart-jboss.sh
--- sh/urr.sh
+++ sh/urr.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd /data/sfsf/workspace/trunk/build-system
-svn update
+svn update --username I854966
 cd /data/sfsf/workspace/trunk/au-V4
-svn update
-~/rebuild-tomcat.sh
+svn update --username I854966
+rebuild-tomcat.sh
 #~/rebuild-jboss.sh
-~/rr-tomcat.sh
+rr-tomcat.sh
 #~/rr-jboss.sh
