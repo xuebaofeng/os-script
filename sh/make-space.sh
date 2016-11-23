19cfe379d32ed34acd2aeb423497f14526b6c5de
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2016-11-22 22:38:18 -0800

    Update make-space.sh

--- sh/make-space.sh
+++ sh/make-space.sh
@@ -1,6 +1,7 @@
-rm -rf ~/.gradle/caches/modules-2/files-2.1/com.successfactors
-rm -rf ~/.ivy2/cache/com.successfactors
+rm -rf ~/.gradle/caches
+rm -rf ~/.ivy2/cache
 rm -rf ~/.IdeaIC2016.2/system
+rm -rf ~/.IdeaIC2016.3/system
 rm -rf /data/sfsf/workspace/trunk/jboss-sfs
 rm -rf /data/sfsf/workspace/trunk/tomcat-sfs
 cd /data/
@@ -8,5 +9,4 @@ rm -rf flash_recovery
 rm -rf .Trash-1004
 cd sfsf
 rm -rf eclipse_workspace
-
 find /data/sfsf/workspace -name target -type d -exec rm -rf {} \;
