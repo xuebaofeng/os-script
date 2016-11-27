0824c0ed28cf31e5513a1aa51ac7dcdfd081fb3d
Baofeng Xue <xbaofeng@gmail.com>
2016-11-26 16:32:13 -0800

    r

--- sh/restart-jboss.sh
+++ sh/restart-jboss.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 export JBOSS_HOME=/data/sfsf/workspace/trunk/jboss-sfs
+cd /data/sfsf/workspace/trunk
+gradle updateJbossSfs
+cd /data/sfsf/workspace/trunk/bizx-docker-dev
+docker-compose up -d hornet
 cd /data/sfsf/workspace/trunk/jboss-sfs/bin
 ./runMain.sh DEBUG
