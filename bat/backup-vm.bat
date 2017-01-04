7774e184d97d793b133382749adafe380531b2a7
Baofeng Xue <xbaofeng@gmail.com>
2017-01-04 12:32:44 -0800

    'backup'

--- bat/backup-vm.bat
+++ bat/backup-vm.bat
@@ -1,11 +1,11 @@
 rem https://my.vmware.com/web/vmware/free#desktop_end_user_computing/vmware_workstation_player/12_0|PLAYER-1200|drivers_tools
 cd %USERPROFILE%
-vmrun suspend Downloads\Bizx2.0.1-b1602a\Bizx2.0.vmx
 
+vmrun suspend Downloads\DEV3.0\DEV3.0.vmx
 
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 echo %mydate%
 
-robocopy Downloads\Bizx2.0.1-b1602a Downloads\%mydate%
+robocopy Downloads\DEV3.0 Downloads\%mydate%
 pause
