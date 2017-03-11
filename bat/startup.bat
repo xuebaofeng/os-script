2b870dc3975c84d9bd0d1c1618ee6160e9e0af0f
Baofeng Xue <xbaofeng@gmail.com>
2017-03-11 12:32:47 -0800

    'backup'

--- bat/startup.bat
+++ bat/startup.bat
@@ -1,3 +1,3 @@
 start "" "c:\PortableApps\PortableApps.com\PortableAppsPlatform.exe"
 start "" "c:\green\syncthing\syncthing.exe"
-start "" "C:\Program Files\Microsoft Office 15\root\office15\outlook.exe"
+start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"
