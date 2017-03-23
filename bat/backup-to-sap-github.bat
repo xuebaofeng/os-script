4e60f61b496523cba1e583c18a750eed0ff89b2f
Baofeng Xue <xbaofeng@gmail.com>
2017-03-23 12:32:44 -0700

    'backup'

--- bat/backup-to-sap-github.bat
+++ bat/backup-to-sap-github.bat
@@ -2,6 +2,7 @@ cd %USERPROFILE%\github\backup
 git pull
 
 copy %USERPROFILE%\Downloads\my-umatrix-rules*.txt .\chrome\umatrix\
+copy C:\data\sfsf\svn\trunk\*.bat .\bat\
 
 git add -A
 git commit -m 'backup'
