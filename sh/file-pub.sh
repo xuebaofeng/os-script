5985c5eccc7120df8c5e52777afef664cb488891
xuebaofeng <xbaofeng@gmail.com>
2017-05-10 21:17:48 -0700

    r

--- sh/file-pub.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-lsof -P | grep ':9999' | awk '{print $2}' | xargs kill -9
-python -m SimpleHTTPServer 9999 &
-ngrok http 9999
--- sh/restart-tomcat.sh
+++ sh/restart-tomcat.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd /data/sfsf/workspace/trunk
+cp build-system/sfs-local-overrides/tomcat-dev-docker-oracle/build.properties build-system/
 gradle updateTomcatSfs
 cd /data/sfsf/workspace/trunk/bizx-docker-dev
 restart-docker.sh
