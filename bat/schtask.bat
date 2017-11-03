f3de7b246778e2434399bfd17033f9d8a393b9ee
Baofeng Xue <xbaofeng@gmail.com>
2017-11-03 12:40:27 -0700

    r

--- bat/schtask.bat
+++ bat/schtask.bat
@@ -3,7 +3,7 @@ schtasks /Delete /TN "backup vm" /F
 schtasks /create /sc WEEKLY /tn "backup vm" /tr "%location%\backup-vm.bat" /d wed /st 12:00
 
 schtasks /Delete /TN "backup os" /F
-schtasks /create /sc daily /tn "backup os" /tr "%location%\backup-os.bat" /st 12:30
+schtasks /create /sc WEEKLY /tn "backup os" /tr "%location%\backup-os.bat" /d MON,TUE,WED  /st 12:30
 
 schtasks /Delete /TN "backup to sap github" /F
-schtasks /create /sc daily /tn "backup to sap github" /tr "%location%\backup-to-sap-github.bat" /st 12:30
+schtasks /create /sc WEEKLY /tn "backup to sap github" /tr "%location%\backup-to-sap-github.bat" /d MON,TUE,WED /st 12:30
