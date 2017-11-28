66237c4f789e26e7695030b1433754af1cfed08a
Baofeng Xue <baofeng.xue@sap.com>
2017-11-28 11:01:03 -0800

    r

---
+++ bat/chrome-cache.bat
@@ -0,0 +1,3 @@
+rd /s /q "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache"
+mkdir r:\chrome-cache
+mklink /J "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache" "r:\chrome-cache"
