fd560f2298e15c54b1d53ae201b8d8e761828463
Baofeng Xue <baofeng.xue@sap.com>
2017-11-28 13:43:56 -0800

    'backup'

--- bat/chrome-cache.bat
+++ bat/chrome-cache.bat
@@ -1,3 +1,8 @@
-rd /s /q "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache"
 mkdir r:\chrome-cache
+mkdir r:\firefox-cache
+mkdir r:\tmp
+mkdir r:\temp
+mkdir r:\u-tmp
+mkdir r:\u-temp
+rd /s /q "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache"
 mklink /J "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache" "r:\chrome-cache"
