6229f4ca1edd3c2b925c2f25d1eb1b5078fe9520
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2017-11-29 12:45:02 -0800

    'backup'

--- bat/chrome-cache.bat
+++ bat/chrome-cache.bat
@@ -4,5 +4,6 @@ mkdir r:\tmp
 mkdir r:\temp
 mkdir r:\u-tmp
 mkdir r:\u-temp
+mkdir r:\idm-temp
 rd /s /q "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache"
 mklink /J "C:\Users\I854966\AppData\Local\Google\Chrome\User Data\Default\Cache" "r:\chrome-cache"
