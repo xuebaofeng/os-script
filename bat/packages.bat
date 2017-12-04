fa920107ea8022841aa318aa5ac94e0b4a0a9d73
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2017-12-04 13:57:28 -0800

    'backup'

---
+++ bat/packages.bat
@@ -0,0 +1,47 @@
+rem choco install -y vcredist2005
+rem choco install -y vcredist2008
+rem choco install -y vcredist2010
+rem choco install -y vcredist2012
+rem choco install -y vcredist2013
+rem choco install -y vcredist2015
+rem choco install -y vcredist2017
+rem choco install -y dotnet4.7
+rem choco install -y directx
+rem choco install -y flashplayerplugin
+
+choco install -y jdk8
+choco install -y curl
+choco install -y wget
+choco install -y android-sdk
+choco install -y git
+choco install -y ant
+choco install -y maven
+choco install -y gradle
+choco install -y intellijidea-community
+
+choco install -y calibre
+choco install -y ditto
+choco install -y dropbox
+choco install -y evernote
+choco install -y everthing
+choco install -y freefilesync
+choco install -y foxitreader
+choco install -y googlechrome
+choco install -y imdisk-toolkit
+choco install -y io-unlocker
+choco install -y keepass
+choco install -y meld
+choco install -y netease-cloudmusic
+choco install -y notepadplusplus
+choco install -y pdfxchangeeditor
+choco install -y pdfxchangeviewer
+choco install -y proxyswitcher
+choco install -y peerblock
+choco install -y slack
+choco install -y teracopy
+choco install -y telegram
+choco install -y vmwareplayer
+choco install -y virtualbox
+choco install -y wechat
+choco install -y wincdemu
+
