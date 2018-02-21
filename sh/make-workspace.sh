ec01fe24dd19f4316088890d84d303571b4e047f
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2018-02-21 12:41:48 -0800

    'backup'

--- sh/make-workspace.sh
+++ sh/make-workspace.sh
@@ -3,10 +3,9 @@ cd /data/sfsf/workspace
 rm -rf ./trunk
 mkdir trunk
 cd trunk
+git clone https://github.wdf.sap.corp/sfengservices/bizx-docker-dev.git
 git clone https://github.wdf.sap.corp/bizx/au-V4.git --depth 1
 git clone https://github.wdf.sap.corp/bizx/build-system.git --depth 1
-
 cd build-system
 gradle configureWorkspace
-
 cp sfs-local-overrides/tomcat-dev-docker-oracle/build.properties ./
