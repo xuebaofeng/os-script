b5aa1a38e3eba3b737cc285ec4d0854dc77eba06
xuebaofeng <Jy6#5ev9Ir3eQqWaEKbv>
2018-03-19 22:16:33 -0700

    r

--- bat/schtask.bat
+++ bat/schtask.bat
@@ -1,6 +1,4 @@
 set location="c:\wbem\github\os-script\bat"
-schtasks /Delete /TN "backup vm" /F
-schtasks /create /sc WEEKLY /tn "backup vm" /tr "%location%\backup-vm.bat" /d wed /st 12:00
 
 schtasks /Delete /TN "backup os" /F
 schtasks /create /sc WEEKLY /tn "backup os" /tr "%location%\backup-os.bat" /d MON,TUE,WED  /st 12:30
