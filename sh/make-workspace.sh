c1dbfa2f4741824926e0b6119654ed821b594b26
xuebaofeng <xbaofeng@gmail.com>
2018-06-11 14:16:59 -0700

    r

--- sh/make-workspace.sh
+++ sh/make-workspace.sh
@@ -8,4 +8,3 @@ git clone https://github.wdf.sap.corp/bizx/au-V4.git --depth 1
 git clone https://github.wdf.sap.corp/bizx/build-system.git --depth 1
 cd build-system
 gradle configureWorkspace -u
-cp sfs-local-overrides/tomcat-dev-docker-oracle/build.properties ./
