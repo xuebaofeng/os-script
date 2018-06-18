926ad0b4f155c8c31509745ea7b2dd0e95ebb3a9
xuebaofeng <Jy6#5ev9Ir3eQqWaEKbv>
2018-06-18 10:59:22 -0700

    'backup'

--- bat/backup-vm.bat
+++ bat/backup-vm.bat
@@ -1,13 +1,13 @@
 rem https://my.vmware.com/web/vmware/free#desktop_end_user_computing/vmware_workstation_player/12_0|PLAYER-1200|drivers_tools
 cd c:\wbem
 
-vmrun suspend DEV3.1\DEV3.1.vmx
+vmrun suspend DEV3.1.2\DEV3.1.2.vmx
 
 @echo off
 
 set mydate=%date:~-4%%date:~4,2%%date:~7,2%
 echo %mydate%
 
-robocopy DEV3.1 f:\backup\vm\%mydate% /xf *.log *.lock *.vmem *.gz *.dmp *.vmss *.vmxf *.vmsd
-robocopy DEV3.1 g:\backup\vm\%mydate% /xf *.log *.lock *.vmem *.gz *.dmp *.vmss *.vmxf *.vmsd
+robocopy DEV3.1.2 f:\backup\vm\%mydate% /xf *.log *.lock *.vmem *.gz *.dmp *.vmss *.vmxf *.vmsd
+robocopy DEV3.1.2 g:\backup\vm\%mydate% /xf *.log *.lock *.vmem *.gz *.dmp *.vmss *.vmxf *.vmsd
 pause
