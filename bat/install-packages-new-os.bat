2e86be0385b3a6b8c9a180ed14ee3ad505e5fb69
xuebaofeng <Jy6#5ev9Ir3eQqWaEKbv>
2018-10-09 13:21:47 -0700

    'backup'

---
+++ bat/install-packages-new-os.bat
@@ -0,0 +1,8 @@
+choco install -y ditto
+choco install -y everthing
+choco install -y listary
+choco install -y freefilesync
+choco install -y flashplayerplugin
+choco install -y honeyview
+choco install -y keepass
+
