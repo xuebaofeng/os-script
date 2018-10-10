b38c38b2087b8f86d64d2eb18c9f612b2f095cab
xuebaofeng <Jy6#5ev9Ir3eQqWaEKbv>
2018-10-10 12:37:58 -0700

    'backup'

--- bat/startup.bat
+++ bat/startup.bat
@@ -1,3 +1,4 @@
 start "" "c:\PortableApps\PortableApps.com\PortableAppsPlatform.exe"
 start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "C:\green\GoldenDict\GoldenDict.exe"
