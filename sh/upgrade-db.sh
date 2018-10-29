cc5758914de7c6000a062b3ea15c0a29b7c922b5
xuebaofeng <Jy6#5ev9Ir3eQqWaEKbv>
2018-10-29 13:09:47 -0700

    'backup'

--- sh/upgrade-db.sh
+++ sh/upgrade-db.sh
@@ -5,7 +5,7 @@ cd /data/sfsf/workspace/trunk/bizx-docker-dev
 #US East region	saas-docker-nsq.mo.sap.corp
 #US West region	saas-docker-dub.mo.sap.corp
 #Germany	saas-docker-rot.mo.sap.corp
-export REGISTRY=saas-docker-dub.mo.sap.corp
+export REGISTRY=saas-docker-nsq.mo.sap.corp
 
 docker-compose pull oracle
 
