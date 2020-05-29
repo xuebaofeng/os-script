2d9d0aa4ac6b4fcd096d77280e06d478279ad172
Baofeng Xue <xbaofeng@gmail.com>
2020-05-28 22:53:23 -0700

    r

--- sh/k8s/k3s.sh
+++ sh/k8s/k3s.sh
@@ -1,15 +1,9 @@
+##/usr/local/bin/k3s-uninstall.sh
+
 cd /shared/k8s/
 export K3S_KUBECONFIG_MODE="644"
 
-FILE=./k3s
-if [ -f "$FILE" ]; then
-    echo "$FILE exist"
-else
-    echo "$FILE does not exist"
-    curl -Lo $FILE https://get.k3s.io
-    chmod +x $FILE
-    $FILE
-fi
+curl -sfL https://get.k3s.io | sh -
 
 ## setup
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
@@ -22,5 +16,5 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 #done
 
 #sudo systemctl restart k3s
-
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+sudo k3s crictl images
+echo "sudo k3s crictl images"
