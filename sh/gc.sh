99db1a091f8121f448963e72a0044f1577081dd1
Baofeng Xue <xbaofeng@gmail.com>
2020-07-27 10:13:09 -0700

    r

---
+++ sh/gc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ $# -gt 0 ]; then
+    echo "Your command line contains $# arguments"
+else
+    echo "Input the repo name, like 'au-V4'"
+    exit;
+fi
+cd ${GRADLE_WORKSPACE}/
+git clone https://github.wdf.sap.corp/bizx/$1.git
