01080bf9b398dd998f0b05ef9d01e71e45fda0e8
Baofeng Xue <xbaofeng@gmail.com>
2020-07-27 10:13:46 -0700

    r

--- sh/gc.sh
+++ sh/gc.sh
@@ -3,7 +3,7 @@ if [ $# -gt 0 ]; then
     echo "Your command line contains $# arguments"
 else
     echo "Input the repo name, like 'au-V4'"
-    exit;
+    exit 1;
 fi
 cd ${GRADLE_WORKSPACE}/
 git clone https://github.wdf.sap.corp/bizx/$1.git
