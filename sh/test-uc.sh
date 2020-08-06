592a293ad523d9ed46d94a95b285cfd0e2d5ceae
Baofeng Xue <xbaofeng@gmail.com>
2020-08-06 09:52:07 -0700

    r

--- sh/test-uc.sh
+++ sh/test-uc.sh
@@ -4,4 +4,11 @@ cw.sh
 cd ${GRADLE_WORKSPACE}/au-upgradecenter/au-upgradecenter-service/
 gradle test --tests com.successfactors.upgradecenter.utils.UpgradeCenterUtilsTest
 gradle test --tests com.successfactors.upgradecenter.service.scheduledjob.UCUpgradeFeatureJobTest
-gradle test --tests com.successfactors.upgradecenter.*
+gradle test --tests com.successfactors.upgradecenter.app.ActionStateManagerTest
+gradle test --tests com.successfactors.upgradecenter.app.UpgradeCenterManagerTest
+gradle test --tests com.successfactors.ucxmlgenerator.app.UCFeatureXMLConfigManagerTest
+gradle test
+
+cd ${GRADLE_WORKSPACE}/au-upgradecenter/au-upgradecenter-ui/
+gradle test --tests com.successfactors.upgradecenter.ui.controller.UpgradeCenterControllerTest
+gradle test
