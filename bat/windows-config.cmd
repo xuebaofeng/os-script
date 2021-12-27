9d75789fbd0626e16b58c87341e36cabe5a362ff
Baofeng Xue <xbaofeng@gmail.com>
2021-12-27 11:52:15 -0800

    r

--- bat/windows-config.cmd
+++ bat/windows-config.cmd
@@ -12,6 +12,7 @@ wmic pagefileset where name="C:\\pagefile.sys" set InitialSize=2048,MaximumSize=
 powercfg.exe /hibernate off
 
 DISM /online /disable-feature /featurename:WindowsMediaPlayer
+DISM /online /disable-feature /featurename:"Teams Machine-Wide Installer"
 rem Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer"
 rem mklink /j "%UserProfile%\OneDrive\backup" C:\Users\I854966\Downloads\
 
@@ -19,3 +20,6 @@ rem mklink /j "%UserProfile%\OneDrive\backup" C:\Users\I854966\Downloads\
 @REM reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google /f
 
 winget uninstall MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy
+
+
+@REM netsh interface ip set address name="Wi-Fi" static 192.168.50.40 255.255.255.0 192.168.50.1
