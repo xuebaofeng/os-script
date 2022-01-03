a01e875863d75c4bfa149ba962fa422ed7be5b0c
Baofeng Xue <xbaofeng@gmail.com>
2022-01-02 22:20:15 -0800

    r

--- bat/k8s-d.bat
+++
@@ -1,6 +0,0 @@
-minikube delete
-choco uninstall -y docker-cli
-choco uninstall -y docker-desktop
-choco uninstall -y minikube
-choco uninstall -y kubernetes-cli
-choco uninstall -y kubernetes-helm
--- bat/r-k8s.bat
+++
@@ -1,2 +0,0 @@
-kubectl apply -f C:\Users\I854966\Downloads\k8s\mybizx-newcode-HANA-local.yaml
-kubectl delete deployment demo-cf
