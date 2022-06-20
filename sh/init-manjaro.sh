c4ed8e7326ffc2ce16e48910d841ab230b86d474
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2022-06-20 16:35:03 -0700

    Update init-manjaro.sh

--- sh/init-manjaro.sh
+++ sh/init-manjaro.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-sudo mkdir /shared
-sudo mount /dev/sdb1 /shared
 
-sudo mount-disk.sh
-
-sudo pacman -S yay --noconfirm
+sudo pacman -Sy yay --noconfirm
 
 yay -S --noconfirm fasd
 yay -S --noconfirm glances
 yay -S --noconfirm ncdu
 yay -S --noconfirm jellyfin
-
-
-yay -S --noconfirm docker
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-sudo docker version
+yay -S --noconfirm tigervnc
