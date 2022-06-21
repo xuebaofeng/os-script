50ae69cb451454024e6c93da46e24190fe4e129f
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2022-06-20 18:00:47 -0700

    Update manjaro_vnc.sh

--- sh/manjaro_vnc.sh
+++ sh/manjaro_vnc.sh
@@ -1,8 +1,13 @@
-tee -a ~/.vnc/config <<EOF
+echo "
 session=xfce
 geometry=2560x1440
 alwaysshared
-EOF
+" > ~/.vnc/config
+
+
+echo ":1=bx" > /etc/tigervnc/vncserver.users
+
+printf "111111\n111111\n\n" | vncpasswd
 
 systemctl enable vncserver@:1
 systemctl start vncserver@:1
