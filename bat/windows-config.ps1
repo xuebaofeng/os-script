5f55036351ecfbaa0dc57397b759051f945b108f
Baofeng Xue <xbaofeng@gmail.com>
2024-05-19 22:10:39 -0700

    na

--- bat/windows-config.ps1
+++ bat/windows-config.ps1
@@ -1,8 +1,5 @@
-
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
 
 get-appxpackage *photos* | remove-appxpackage
 get-appxpackage *zunevideo* | remove-appxpackage
 get-appxpackage Microsoft.GamingServices | remove-AppxPackage -allusers
-
-Set-MpPreference -DisableRealtimeMonitoring $true
