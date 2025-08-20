a58913221ea243ce09baaa9d66fd07f4b339f89c
Baofeng Xue <xbaofeng@gmail.com>
2025-08-19 19:41:13 -0700

    na

--- bat/make_win_start.bat
+++ bat/make_win_start.bat
@@ -1,3 +1,3 @@
-copy "C:\github\os-script\bat\win_start.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
+rem copy "C:\github\os-script\bat\win_start.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
 copy "C:\github\os-script\bat\win_start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 
--- bat/win_start.bat
+++ bat/win_start.bat
@@ -1,3 +1,4 @@
 @echo off
 start "" "C:\ProgramData\chocolatey\bin\syncthing.exe"
+start "" "C:\ProgramData\chocolatey\bin\localsend_app.exe"
 start "" "C:\green\jellyfin\jellyfin.exe"
