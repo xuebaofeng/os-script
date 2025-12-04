26841b1493c5328609853d4698c08dff4e45ee59
Shawn Xue <xbaofeng@gmail.com>
2025-12-03 22:01:45 -0800

    r

--- bat/win_start.bat
+++ bat/win_start.bat
@@ -1,4 +1,4 @@
 @echo off
+@REM start "" "C:\ProgramData\chocolatey\bin\localsend_app.exe"
 start "" "C:\ProgramData\chocolatey\bin\syncthing.exe"
-start "" "C:\ProgramData\chocolatey\bin\localsend_app.exe"
-start "" "C:\green\jellyfin\jellyfin.exe"
+start "" "C:\Program Files\Jellyfin\Server\jellyfin.exe"
--- bat/windows-config.bat
+++ bat/windows-config.bat
@@ -2,15 +2,19 @@ sc stop "WSearch"
 sc config "WSearch" start= disabled
 
 wmic computersystem where name="%computername%" set AutomaticManagedPagefile=False
-wmic pagefileset where name="C:\\pagefile.sys" set InitialSize=2048,MaximumSize=2048
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" `
+    -Name "PagingFiles" `
+    -Value "C:\pagefile.sys 2048 2048"
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" `
+    -Name "AutoManagedPagefile" `
+    -Value 0
 
 powercfg.exe /hibernate off
 
-takeown /F "C:\$Windows.~BT\*" /A /R /D Y
-icacls "C:\$Windows.~BT\*.*" /grant *S-1-5-32-544:F /T /C /Q
-RD /S /Q "C:\$Windows.~BT"
-
-tzutil /s "Pacific Standard Time"
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f
-taskkill /f /im explorer.exe & start explorer.exe
+@REM takeown /F "C:\$Windows.~BT\*" /A /R /D Y
+@REM icacls "C:\$Windows.~BT\*.*" /grant *S-1-5-32-544:F /T /C /Q
+@REM RD /S /Q "C:\$Windows.~BT"
+@REM tzutil /s "Pacific Standard Time"
+@REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f
+@REM taskkill /f /im explorer.exe & start explorer.exe
