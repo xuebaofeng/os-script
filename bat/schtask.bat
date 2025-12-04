e7ed1057957b797147605c6517e521a185500737
Shawn Xue <xbaofeng@gmail.com>
2025-12-04 09:02:34 -0800

    r

--- bat/schtask.bat
+++ bat/schtask.bat
@@ -1,2 +1,5 @@
 schtasks /Delete /TN "backup DCIM" /F
 schtasks /create /sc daily /tn "backup DCIM" /tr "C:\green\bin\backup-dcim.bat" /st 22:55
+
+schtasks /delete /tn "DailyShutdown" /f
+schtasks /create /tn "DailyShutdown" /tr "shutdown -s -f -t 0" /sc daily /st 22:25
